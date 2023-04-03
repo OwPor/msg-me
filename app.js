a0ea22fb97bd37cc19fe40ce2f389ef3314fe97b
Ray Vincent DS. Concepcion <rayvincentconcepcion@gmail.com>
2023-04-04 02:17:16 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// Initialize Firebase
+const firebaseConfig = {
+	apiKey: "AIzaSyCQaGLn4HCbesVcHvPtTcDKZzVYqU08BPU",
+	authDomain: "anon-mess.firebaseapp.com",
+	projectId: "anon-mess",
+	storageBucket: "anon-mess.appspot.com",
+	messagingSenderId: "54977689429",
+	appId: "1:54977689429:web:f58d80087065cba005e1dc"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth =  firebase.auth();
+
+//signIN function
+function  signIn(){
+	var email = document.getElementById("email");
+	var password  = document.getElementById("password");
+	const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+	promise.catch(e=>alert(e.message));
+  
+}
+
+
+//signOut
+
+function signOut(){
+	auth.signOut();
+	alert("SignOut Successfully from System");
+}
+
+//active user to homepage
+firebase.auth().onAuthStateChanged((user)=>{
+	if(user){
+		var email = user.email;
+		alert("Successfully logged in "+email+"!");
+		// Redirect to the desired page
+		window.location.href = "Admin/index.html";
+	
+	}else{
+		alert("No Active user Found")
+	}
+});
