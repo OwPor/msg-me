329a6f2d06c93b3ab6d91e594613cf5c24e308b0
Ray Vincent DS. Concepcion <rayvincentconcepcion@gmail.com>
2023-04-04 10:24:03 +0800

    Add files via upload

--- Admin/app.js
+++ Admin/app.js
@@ -16,7 +16,7 @@ const auth =  firebase.auth();
 function signOut(){
 	auth.signOut();
 	alert("Logged out successfully from system!");
-	window.location.href = "https://rvthemoves.github.io/msg-me";
+	window.location.replace("https://rvthemoves.github.io/msg-me/");
 }
 
 // Get a reference to the database service
