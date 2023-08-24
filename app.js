08910e0ef54ed1294f9652aeb77e8a49f6df0765
Ray Vincent DS. Concepcion <rayvincentconcepcion@gmail.com>
2023-08-25 00:42:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ firebase.initializeApp(firebaseConfig);
 
 const auth =  firebase.auth();
 
-//signIN function
+//signIn function
 function signIn(){
-	var email = document.getElementById("email");
-	var password  = document.getElementById("password");
-	const promise = auth.signInWithEmailAndPassword(email.value,password.value);
-	promise.catch(e=>alert(e.message));
-  
+	var email = document.getElementById("email").value;
+	var password  = document.getElementById("password").value;
+	const promise = auth.signInWithEmailAndPassword(email, password);
+	promise.catch(e => alert(e.message));
 }
+
 
 
 //signOut
